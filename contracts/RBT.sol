8b82dc0b582f996bbeaf9c184214e281fdfe68d8
motxx <motohiko.ave@gmail.com>
2022-07-13 18:11:49 +0900

    feat: make RBT support DynamicShares

--- contracts/RBT.sol
+++ contracts/RBT.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC165 } from "@openzeppelin/contracts/interfaces/IERC165.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { ISharesReceiver } from "./finance/ISharesReceiver.sol";
 import { IRBT } from "./tokens/IRBT.sol";
 
 contract RBT is ERC20, Ownable, IRBT {
+    using Address for address;
+
     constructor()
         ERC20("RoyaltyBasedToken", "RBT")
     {
@@ -25,5 +30,21 @@ contract RBT is ERC20, Ownable, IRBT {
         onlyOwner
     {
         _burn(account, amount);
+    }
+
+    function _afterTokenTransfer(
+        address /* from */,
+        address to,
+        uint256 amount
+    )
+        internal
+        override
+    {
+        if (to.isContract()) {
+            if (IERC165(to).supportsInterface(type(ISharesReceiver).interfaceId)) {
+                // 送信先でトークンの配分を行う
+                ISharesReceiver(to).onERC20SharesReceived(amount);
+            }
+        }
     }
 }
