e0452d651cc4ee311079086a722abe440923f1b8
motxx <motohiko.ave@gmail.com>
2022-07-13 20:17:14 +0900

    doc: add basic SangoGraph doc

--- contracts/SangoGraph.sol
+++ contracts/SangoGraph.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.7;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ISangoGraph } from "./ISangoGraph.sol";
 
+/**
+ * @dev SangoContent のリレーショングラフをシングルトン的に管理するコントラクト
+ */
 contract SangoGraph is ISangoGraph {
     mapping (address => mapping (address => uint32)) private _graph;
     IERC20 private _rbt;
