c3d63a403c3193e6530b613360a804ef1d1facef
motxx <motohiko.ave@gmail.com>
2022-07-14 15:13:22 +0900

    test: add tests for proportions

--- test/RBTProportions.ts
+++ test/RBTProportions.ts
@@ -6,18 +6,28 @@ import { Contract } from "ethers";
 chai.use(solidity);
 
 describe("RBTProportions", () => {
-  let rbtShares: Contract;
+  let rbtProp: Contract;
 
   beforeEach(async () => {
     const RBT = await ethers.getContractFactory("RBT");
     const rbt = await RBT.deploy();
-    const RBTShares = await ethers.getContractFactory("setRBTProportions");
-    rbtShares = await RBTShares.deploy(rbt);
-    await rbtShares.deployed();
+    const RBTProportions = await ethers.getContractFactory("RBTProportions");
+    rbtProp = await RBTProportions.deploy(rbt.address);
+    await rbtProp.deployed();
   });
 
   describe("setRBTProportions", () => {
-    it("Should set correct proportions", async () => {
+    it("Should set correct proportions (no treasury)", async () => {
+      await rbtProp.setRBTProportions(6000, 2000, 1500, 500);
+      expect(await rbtProp.creatorProportion()).to.equal(6000);
+      expect(await rbtProp.cetBurnerProportion()).to.equal(2000);
+      expect(await rbtProp.cbtStakerProportion()).to.equal(1500);
+      expect(await rbtProp.primaryProportion()).to.equal(500);
+    });
+
+    it("Should set correct proportions (treasury exists)", async () => {
+      await rbtProp.setRBTProportions(6000, 2000, 1000, 900);
+      expect(await rbtProp.treasuryProportion()).to.equal(100);
     });
   });
 });
