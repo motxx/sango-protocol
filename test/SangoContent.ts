4d286905f5abb1c39a9f6ad48e17b54a8df02f2a
motxx <motohiko.ave@gmail.com>
2022-07-15 19:30:02 +0900

    test: fix SangoContent constructor

--- test/SangoContent.ts
+++ test/SangoContent.ts
@@ -2,35 +2,50 @@ import chai, { assert, expect } from "chai";
 import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
 import { Contract } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 chai.use(solidity);
 
 describe("SangoContent", async () => {
-  let deploySango: (primaries: string[], shares: number[]) => Promise<Contract>;
+  type DeploySango = (creators: string[], creatorShares: number[],
+    primaries: string[], primaryShares: number[]) => Promise<Contract>;
+
+  let deploySango: DeploySango;
+
+  let s1: SignerWithAddress;
 
   beforeEach(async () => {
+    [, s1] = await ethers.getSigners();
+
     const RBT = await ethers.getContractFactory("RBT");
     const rbt = await RBT.deploy();
-    deploySango = async (primaries: string[], shares: number[]) => {
+
+    deploySango = async (creators: string[], creatorShares: number[],
+        primaries: string[], primaryShares: number[]) => {
       const SangoContent = await ethers.getContractFactory("SangoContent");
-      const sango = await SangoContent.deploy(rbt.address, primaries, shares);
+      const sango = await SangoContent.deploy(rbt.address, creators, creatorShares, primaries, primaryShares);
       await sango.deployed();
       return sango;
     };
   });
 
   it("Should construct DAG", async () => {
-    const first = await deploySango([], []);
-    const second = await deploySango([first.address], [100]);
-    const third = await deploySango([first.address, second.address], [200, 300]);
+    const first = await deploySango([s1.address], [1], [], []);
+    const second = await deploySango([s1.address], [1], [first.address], [100]);
+    const third = await deploySango([s1.address], [1], [first.address, second.address], [200, 300]);
     assert.deepEqual(await first.getPrimaries(), []);
     assert.deepEqual(await second.getPrimaries(), [first.address]);
     assert.deepEqual(await third.getPrimaries(), [first.address, second.address]);
   });
 
-  it("Should not have duplicate address (edges)", async () => {
-    const first = await deploySango([], []);
-    await expect(deploySango([first.address, first.address], [100, 200])).to.revertedWith(
-      "VM Exception while processing transaction: reverted with reason string 'SangoContent: already primary'");
+  it("Should not have duplicate creators", async () => {
+    await expect(deploySango([s1.address, s1.address], [1, 2], [], [])).to.revertedWith(
+      "VM Exception while processing transaction: reverted with reason string 'DynamicShares: the payee already exists'");
+  });
+
+  it("Should not have duplicate primaries", async () => {
+    const first = await deploySango([s1.address], [1], [], []);
+    await expect(deploySango([s1.address], [1], [first.address, first.address], [100, 200])).to.revertedWith(
+      "VM Exception while processing transaction: reverted with reason string 'DynamicShares: the payee already exists'");
   });
 });
