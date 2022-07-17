a9058a8c8babb82e175764dfcfdbe6e46f859a18
motxx <motohiko.ave@gmail.com>
2022-07-17 17:32:36 +0900

    test: add distribution primaries test

--- test/ExchangeService.ts
+++ test/ExchangeService.ts
@@ -118,3 +118,79 @@ describe("ExchangeService", () => {
     });
   });
 });
+
+describe("distribute on DAG", () => {
+  let exchangeService: Contract;
+  let rbt: Contract;
+  let rbtAddress: string;
+
+  beforeEach(async () => {
+    const ExchangeService = await ethers.getContractFactory("ExchangeService");
+    exchangeService = await ExchangeService.deploy();
+    await exchangeService.deployed();
+    rbtAddress = await exchangeService.rbt();
+    rbt = await ethers.getContractAt("RBT", rbtAddress);
+  });
+
+  it("Should verify balance of the creators of primary contents", async () => {
+    /**
+     *         ┌─┐
+     *      ┌──┴C6──┐
+     *     ┌▼┐     ┌▼┐
+     *  ┌──┴C4──┬──┴C5──┐
+     * ┌▼┐     ┌▼┐     ┌▼┐
+     * └C1     └C2     └C3
+     */
+
+    const [, s1, s2, s3, s4, s5, s6] = await ethers.getSigners();
+
+    const deploySango_ = async (
+      signer: SignerWithAddress,
+      creators: string[],
+      creatorShares: number[],
+      primaries: string[],
+      primaryShares: number[],
+      primaryProp: number,
+    ) => {
+      return await deploySangoBy(signer, {
+        rbtAddress,
+        creators,
+        creatorShares,
+        primaries,
+        primaryShares,
+        creatorProp: 10000 - primaryProp,
+        cetBurnerProp: 0,
+        cbtStakerProp: 0,
+        primaryProp,
+      });
+    };
+
+    const C1 = await deploySango_(s1, [s1.address], [1], [], [], 0);
+    const C2 = await deploySango_(s2, [s2.address], [1], [], [], 0);
+    const C3 = await deploySango_(s3, [s3.address], [1], [], [], 0);
+    const C4 = await deploySango_(s4, [s4.address], [1], [C1.address, C2.address], [2, 1], 1000);
+    const C5 = await deploySango_(s5, [s5.address], [1], [C2.address, C3.address], [1, 2], 1000);
+    const C6 = await deploySango_(s6, [s6.address], [1], [C4.address, C5.address], [1, 1], 2000);
+
+    await exchangeService.mint(C6.address, 10000);
+    await C6.releaseCreatorShares(s6.address);
+    expect(await rbt.balanceOf(s6.address)).to.equal(8000);
+    await C6.releasePrimaryShares(C4.address);
+    await C6.releasePrimaryShares(C5.address);
+    expect(await rbt.balanceOf(C4.address)).to.equal(1000);
+    expect(await rbt.balanceOf(C5.address)).to.equal(1000);
+    expect(await rbt.balanceOf(s4.address)).to.equal(0);
+    expect(await rbt.balanceOf(s5.address)).to.equal(0);
+    await C4.releaseCreatorShares(s4.address);
+    await C5.releaseCreatorShares(s5.address);
+    expect(await rbt.balanceOf(s4.address)).to.equal(900);
+    expect(await rbt.balanceOf(s5.address)).to.equal(900);
+    await C4.releasePrimaryShares(C1.address);
+    await C4.releasePrimaryShares(C2.address);
+    await C5.releasePrimaryShares(C2.address);
+    await C5.releasePrimaryShares(C3.address);
+    expect(await rbt.balanceOf(C1.address)).to.equal(67);
+    expect(await rbt.balanceOf(C2.address)).to.equal(67);
+    expect(await rbt.balanceOf(C3.address)).to.equal(66);
+  });
+});
