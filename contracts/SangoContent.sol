536b066ab8ba176f53a0225c8d9c5083309e8d1f
motxx <motohiko.ave@gmail.com>
2022-07-24 12:52:00 +0900

    feat: add CET interfaces

--- contracts/SangoContent.sol
+++ contracts/SangoContent.sol
@@ -6,9 +6,14 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { ISangoContent } from "./ISangoContent.sol";
 import { RBTProportions } from "./RBTProportions.sol";
+import { CET } from "./CET.sol";
+import { IExcitingModule } from "./components/IExcitingModule.sol";
 
 contract SangoContent is ISangoContent, Ownable, RBTProportions {
     using Address for address;
+
+    IExcitingModule[] private _excitingModules;
+    CET private _cet;
 
     constructor(
         IERC20 rbt,
@@ -19,13 +24,16 @@ contract SangoContent is ISangoContent, Ownable, RBTProportions {
         uint32 creatorProp,
         uint32 cetBurnerProp,
         uint32 cbtStakerProp,
-        uint32 primaryProp
+        uint32 primaryProp,
+        string memory cetName,
+        string memory cetSymbol
     )
         RBTProportions(rbt)
     {
         _getCreatorShares().initPayees(creators, creatorShares);
         _getPrimaryShares().initPayees(primaries, primaryShares);
         setRBTProportions(creatorProp, cetBurnerProp, cbtStakerProp, primaryProp);
+        _cet = new CET(cetName, cetSymbol);
     }
 
     /// @inheritdoc ISangoContent
@@ -47,6 +55,15 @@ contract SangoContent is ISangoContent, Ownable, RBTProportions {
         );
     }
 
+    /// @inheritdoc ISangoContent
+    function setExcitingModules(IExcitingModule[] calldata excitingModules)
+        external
+        override
+        /* onlyGovernance */
+    {
+        _excitingModules = excitingModules;
+    }
+
     // #############################
     // ## Contents Royalty Graph  ##
     // #############################
@@ -59,5 +76,56 @@ contract SangoContent is ISangoContent, Ownable, RBTProportions {
         returns (address[] memory)
     {
         return _getPrimaryShares().allPayees();
+    }
+
+    // #############################
+    // ## Contents Excited Token  ##
+    // #############################
+
+    /// @inheritdoc ISangoContent
+    function approveCETReceiver(address account)
+        external
+        override
+        /* onlyGovernance */
+    {
+        _cet.approveCETReceiver(account);
+    }
+
+    function cet()
+        external
+        view
+        returns (CET)
+    {
+        return _cet;
+    }
+
+    /// @inheritdoc ISangoContent
+    function mintCET(address account)
+        external
+        override
+        /* onlyGovernance */
+    {
+        for (uint32 i = 0; i < _excitingModules.length;) {
+            _excitingModules[i].mintCET(_cet, account);
+            unchecked { i++; }
+        }
+    }
+
+    /// @inheritdoc ISangoContent
+    function burnCET(uint256 amount)
+        external
+        override
+    {
+        _cet.burn(_msgSender(), amount);
+    }
+
+    /// @inheritdoc ISangoContent
+    function getBurnedCET(address account)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return _cet.burnedAmount(account);
     }
 }
