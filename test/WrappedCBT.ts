b9e0b3d98b1887a02a593691248fc1213adf8318
motxx <motohiko.ave@gmail.com>
2022-07-25 16:09:47 +0900

    test: add more test cases for WrappedCBT

--- test/WrappedCBT.ts
+++ test/WrappedCBT.ts
@@ -8,12 +8,13 @@ chai.use(solidity);
 
 describe("Wrapped CBT", async () => {
   let cbtWallet: SignerWithAddress;
+  let owner: SignerWithAddress;
   let s1: SignerWithAddress;
   let cbt: Contract;
   let wCBT: Contract;
 
   beforeEach(async () => {
-    [, cbtWallet, s1] = await ethers.getSigners();
+    [owner, cbtWallet, s1] = await ethers.getSigners();
     const CBT = await ethers.getContractFactory("CBT");
     cbt = await CBT.deploy(cbtWallet.address);
 
@@ -27,5 +28,50 @@ describe("Wrapped CBT", async () => {
     await wCBT.connect(s1).purchase(100);
     expect(await cbt.balanceOf(s1.address)).to.equals(900);
     expect(await wCBT.balanceOf(s1.address)).to.equals(100);
+  });
+
+  it("Should not purchase if less than minAmount", async () => {
+    await wCBT.setMinAmount(200);
+    await cbt.connect(cbtWallet).transfer(s1.address, 1000);
+    await cbt.connect(s1).approve(wCBT.address, 1000);
+    await expect(wCBT.connect(s1).purchase(199)).to.revertedWith(
+      "VM Exception while processing transaction: reverted with reason string 'WrappedCBT: less than minAmount'");
+    await wCBT.connect(s1).purchase(200);
+  });
+
+  it("Should not purchase if less than minAmount", async () => {
+    await wCBT.setMinAmount(200);
+    await cbt.connect(cbtWallet).transfer(s1.address, 1000);
+    await cbt.connect(s1).approve(wCBT.address, 1000);
+    await expect(wCBT.connect(s1).purchase(199)).to.revertedWith(
+      "VM Exception while processing transaction: reverted with reason string 'WrappedCBT: less than minAmount'");
+    await wCBT.connect(s1).purchase(200);
+  });
+
+  it("Should redeem", async () => {
+    await cbt.connect(cbtWallet).transfer(s1.address, 1000);
+    await cbt.connect(s1).approve(wCBT.address, 1000);
+    await wCBT.connect(s1).purchase(100);
+    await wCBT.redeem(s1.address);
+    expect(await cbt.balanceOf(s1.address)).to.equals(1000);
+    expect(await wCBT.balanceOf(s1.address)).to.equals(0);
+  });
+
+  it("Should withdraw", async () => {
+    await cbt.connect(cbtWallet).transfer(s1.address, 1000);
+    await cbt.connect(s1).approve(wCBT.address, 1000);
+    await wCBT.connect(s1).purchase(100);
+    await wCBT.withdraw(20);
+    expect(await cbt.balanceOf(owner.address)).to.equals(20);
+    expect(await cbt.balanceOf(wCBT.address)).to.equals(80);
+  });
+
+  it("Should not redeem if lack of CBT", async () => {
+    await cbt.connect(cbtWallet).transfer(s1.address, 1000);
+    await cbt.connect(s1).approve(wCBT.address, 1000);
+    await wCBT.connect(s1).purchase(100);
+    await wCBT.withdraw(20);
+    await(expect(wCBT.redeem(s1.address))).to.revertedWith(
+      "VM Exception while processing transaction: reverted with reason string 'WrappedCBT: lack of CBT balance'");
   });
 });
